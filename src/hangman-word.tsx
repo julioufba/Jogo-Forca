a57cfffc18b3a2540f4a48cb4b2a1d175a30f2d5
Júlio César <juliomoreira@ufba.br>
2024-02-02 12:13:38 -0300

    final

--- src/hangman-word.tsx
+++ src/hangman-word.tsx
@@ -2,13 +2,21 @@ import styled from "styled-components"
 
 const Wrapper = styled.div `
 display: flex;
-gap: 0.6rem;
+gap: 1rem;
 justify-content:center;
 font-size: 6rem;
 text-transform: uppercase;
 font-weight: bold;
 font-family: 'Arial';
-;
+width:100%;
+@media(max-width:700px){
+    gap:0.05rem;
+    font-size:3rem;
+    width    :100%;
+    margin-left:15px;
+    
+}
+
 `
 
 interface HangmanWordProps{
@@ -21,7 +29,7 @@ export default function HangmanWord({word, GuessedLetters,reveal}:HangmanWordPro
     return (
         
         <Wrapper > {word.split("").map((letra, index) => (
-            <span style={{borderBottom:'0.25rem solid black', height:"", minWidth:'70px'}}key = {index}> 
+            <span style={{borderBottom:'0.25rem solid black', height:"", minWidth:'50px'}}key = {index}> 
             <span style={{visibility:GuessedLetters.includes(letra)||reveal? 'visible': "hidden", color:!GuessedLetters.includes(letra)? 'red':"black"}}>{letra}</span></span>
             
         ))} </Wrapper>
